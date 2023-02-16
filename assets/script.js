d78c049d1b1c5d01e100b47a255ca83edc7981b4
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-02-16 15:18:20 -0400

    early structure

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var APIKey = "e0eb50d18300e0e9f4c2b8a9758ede2a";
+
+//Weather request URL
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e0eb50d18300e0e9f4c2b8a9758ede2a
+
+
+// Where to make the API call.
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+//const myObject = {
+//    myKey: ['value1', 'value2']
+//  };
+  
+
+//Reading Excel Files
+const excelURL = 
